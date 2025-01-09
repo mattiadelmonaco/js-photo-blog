a9c0c425bbd445fbc7183abe0f5f35b6dabe14e5
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-09 17:03:45 +0100

    fix: fix script

--- js/script.js
+++ js/script.js
@@ -4,35 +4,38 @@
  * function to generate 6 photocard (images by JSON placeholder)
  */
 const photoCard = () => {
-    for (let i = 0; i < 6; i++) {
         axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         .then(response => {
-            const imageTitle = response.data[i].title
-            const imageUrl = response.data[i].url
+            const photosData = response.data
+
+            for (let i = 0; i < photosData.length; i++) {
+                const imageTitle = photosData[i].title;
+                const imageUrl = photosData[i].url;
             
             colPhotoElm.innerHTML += `
-            <div id="col-photo" class="col">
+            <div class="col">
                 <img class="col--pin" src="img/pin.svg" alt="Photo's pin">
-                    <div id="container-image" class="col--image">
+                    <div class="col--image">
                         <img class="loading col--image__fit" src="https://www.icegif.com/wp-content/uploads/2023/07/icegif-1265.gif" alt="loading">
                         <img class="col--image__fit" src="${imageUrl}" alt="${imageTitle}">
                     </div>
-                <p id="title-image" class="col--image--title">${imageTitle}</p>
+                <p class="col--image--title">${imageTitle}</p>
             </div>
             `
+            }
 
         }).finally(() => {
             const loadingElms = document.querySelectorAll(".loading")
             loadingElms.forEach(loadingElm => loadingElm.classList.add("hidden"))
         })
 }
-}
+
 
 // DOM ELEMENTS 
 
 const colPhotoElm = document.getElementById("col-photo")
-const containerImageElm = document.getElementById("container-image")
-const titleImageElm = document.getElementById("title-image")
+const containerImageElm = document.querySelectorAll(".col--image")
+const titleImageElm = document.querySelectorAll(".col--image--title")
 
 // EVENTS
 
