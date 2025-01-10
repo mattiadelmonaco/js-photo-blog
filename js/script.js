dbb6a7d3410a688f097bd672919c0f3928c58b9e
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-10 17:23:32 +0100

    fix: fix background's style and add comments

--- js/script.js
+++ js/script.js
@@ -12,34 +12,33 @@ const photoCard = () => {
                 const imageTitle = photosData[i].title;
                 const imageUrl = photosData[i].url;
             
-            colPhotoElm.innerHTML += `
-            <div class="col">
-                <img class="col--pin" src="img/pin.svg" alt="Photo's pin">
-                    <div class="col--image">
-                        <img class="loading col--image__fit" src="https://www.icegif.com/wp-content/uploads/2023/07/icegif-1265.gif" alt="loading">
-                        <img class="col--image__fit photo" src="${imageUrl}" alt="${imageTitle}">
+            cardPhotoElm.innerHTML += `
+            <div class="card">
+                <img class="card--pin" src="img/pin.svg" alt="Photo's pin">
+                    <div class="card--image">
+                        <img class="loading card--image__fit" src="https://www.icegif.com/wp-content/uploads/2023/07/icegif-1265.gif" alt="loading">
+                        <img class="card--image__fit photo" src="${imageUrl}" alt="${imageTitle}">
                     </div>
-                <p class="col--image--title">${imageTitle}</p>
+                <p class="card--image--title">${imageTitle}</p>
             </div>
             `
             }
 
-        const cards = document.querySelectorAll(".col")
+        const cards = document.querySelectorAll(".card")
         const overlayImage = overlayElm.querySelector("img")
         
         cards.forEach((card) => {
             card.addEventListener("click", () => {
         
-                const imgSrc = card.querySelector(".photo")
-                overlayImage.src = imgSrc.src
+                const img = card.querySelector(".photo")
+                overlayImage.src = img.src
+                overlayImage.alt = img.alt
 
                 overlayElm.classList.remove("hidden")
             })
         })
 
-        overlayBtn.addEventListener("click", () => {
-            overlayElm.classList.add("hidden")
-        })
+        overlayBtn.addEventListener("click", () => overlayElm.classList.add("hidden"))
 
         
         // to close overlay also with click on background
@@ -60,9 +59,9 @@ const photoCard = () => {
 
 // DOM ELEMENTS 
 
-const colPhotoElm = document.getElementById("col-photo")
-const containerImageElm = document.querySelectorAll(".col--image")
-const titleImageElm = document.querySelectorAll(".col--image--title")
+const cardPhotoElm = document.getElementById("card-photo")
+const containerImageElm = document.querySelectorAll(".card--image")
+const titleImageElm = document.querySelectorAll(".card--image--title")
 const overlayElm = document.getElementById("overlay")
 const overlayBtn = document.getElementById("overlay-btn")
 const overlayEmpty = document.getElementById("overlay-empty")
